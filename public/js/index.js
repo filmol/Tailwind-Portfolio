c31f6ffa0f48093b2607ca07f2c30849e1c48677
Filip-M <filip.moltzer@gmail.com>
2021-02-26 23:40:29 +0100

    Transition V1 completed with Parcel

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,28 @@ const H = new Highway.Core({
         default: Fade
     }
 });
+
+      // Write function that works callable % for each bar.
+      let progress = 0;
+      let invervalSpeed = 1;
+      let incrementSpeed = 0.4;
+      let todaysDate = new Date()
+      let graduationDay = new Date("June 30, 2022")
+      let msPerDay = 24 * 60 * 60 * 1000 ; // Number of milliseconds per day
+      let daysLeft = (graduationDay.getTime() - todaysDate.getTime()) / msPerDay;
+      daysLeft = Math.round(daysLeft);
+      let completed = ((1060-daysLeft)/1060)*100
+      completed = completed.toFixed(0)
+      document.addEventListener("DOMContentLoaded", function(){
+          let bar = document.getElementById('bar');
+          let progressInterval = setInterval(function(){
+              progress += incrementSpeed;
+              bar.style.width = progress + "%";
+              if(progress >= completed){
+                  clearInterval(progressInterval);
+              }
+          }, invervalSpeed);
+      });
+
+      // document.getElementById("grad").innerHTML += " " + daysLeft + " days"
+      //   document.getElementById("grad").innerHTML += "University studies completed: " + completed + "%"
--- public/js/transition.js
+++ public/js/transition.js
@@ -1,6 +1,47 @@
 import Highway from '@dogstudio/highway';
 import {TimelineLite} from 'gsap';
 
+let addBar = function(){
+
+    let progress = 0;
+    let invervalSpeed = 1;
+    let incrementSpeed = 0.4;
+    let todaysDate = new Date()
+    let graduationDay = new Date("June 30, 2022")
+    let msPerDay = 24 * 60 * 60 * 1000 ; // Number of milliseconds per day
+    let daysLeft = (graduationDay.getTime() - todaysDate.getTime()) / msPerDay;
+    daysLeft = Math.round(daysLeft);
+    let completed = ((1060-daysLeft)/1060)*100
+    completed = completed.toFixed(0)
+
+    let bar = document.getElementById('bar');
+    let progressInterval = setInterval(function(){
+        progress += incrementSpeed;
+        bar.style.width = progress + "%";
+        if(progress >= completed){
+            clearInterval(progressInterval);
+        }
+    }, invervalSpeed);
+
+    // document.getElementById("grad").innerHTML += "University studies completed: " + completed + "%"
+}
+
+let addProject = function(){
+    setTimeout(function(){
+        var replacers = document.querySelectorAll('[data-replace]');
+        for(var i=0; i<replacers.length; i++){
+            console.log('hit here2');
+            let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
+            Object.keys(replaceClasses).forEach(function(key) {
+                replacers[i].classList.remove(key);
+                replacers[i].classList.add(replaceClasses[key]);
+            });
+        }
+    }, 1);
+};
+
+
+
 class Fade extends Highway.Transition{
     in({from, to, done}){
     const tl = new TimelineLite();
@@ -8,18 +49,27 @@ class Fade extends Highway.Transition{
     .fromTo( 
     to,
     0.5,
-    {height:'2vh'},
+    {height:'0vh'},
     {height:'90vh', top:'10%', 
     onComplete: function(){
+       if (to.classList.contains("home")) {
+           addBar();
+        } 
+       if (to.classList.contains("projects")) {
+           addProject();
+        } 
+
        from.remove();
        done(); 
     }}
     )
     .fromTo(to.children[0], 2, {opacity: 0},{opacity: 1})
+    .fromTo(to.children[1], 2, {opacity: 0},{opacity: 1})
     }
     out({from,done}){
         done();
     }
 }
+
 
 export default Fade;
