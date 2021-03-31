7f0536eaf5624712d15df84f7cfc977bbedba5d0
Filip-M <filip.moltzer@gmail.com>
2021-04-01 00:09:38 +0200

    Deleted unused files

--- public/app.js
+++ public/app.js
@@ -1,39 +1,39 @@
-// About page rendering
-var aboutIcon = document.querySelector("#aboutIcon");
-aboutIcon.addEventListener("click", showAbout, false);
-aboutIcon.addEventListener("scroll", showAbout, false);
+// // About page rendering
+// var aboutIcon = document.querySelector("#aboutIcon");
+// aboutIcon.addEventListener("click", showAbout, false);
+// aboutIcon.addEventListener("scroll", showAbout, false);
 
-var aboutPage = document.querySelector("#aboutPage");
+// var aboutPage = document.querySelector("#aboutPage");
 
-function showAbout(e) {
-    var gray = document.querySelector("#gray");
-    var lightGray = document.querySelector("#light-gray");
+// function showAbout(e) {
+//     var gray = document.querySelector("#gray");
+//     var lightGray = document.querySelector("#light-gray");
 
-    setTimeout(addGray, 0)
-    setTimeout(addLightGray, 200)
-    setTimeout(addAbout, 400)
+//     setTimeout(addGray, 0)
+//     setTimeout(addLightGray, 200)
+//     setTimeout(addAbout, 400)
 
-    function addGray() {
-        gray.classList.remove("translate-y-full");
-    }   
-    function addLightGray() {
-        lightGray.classList.remove("translate-y-full");
-    }   
-    function addAbout() {
-        aboutPage.classList.remove("translate-y-full");
-        container.style.display = "none";
-        document.body.style.overflow = "hidden";
-        console.log("Hej")
-    }
-}
+//     function addGray() {
+//         gray.classList.remove("translate-y-full");
+//     }   
+//     function addLightGray() {
+//         lightGray.classList.remove("translate-y-full");
+//     }   
+//     function addAbout() {
+//         aboutPage.classList.remove("translate-y-full");
+//         container.style.display = "none";
+//         document.body.style.overflow = "hidden";
+//         console.log("Hej")
+//     }
+// }
 
-setInterval(letsWave, 4000)
-var wave = document.querySelector("#wave");
+// setInterval(letsWave, 4000)
+// var wave = document.querySelector("#wave");
 
-function removeRotate() {
-    wave.classList.remove("rotate-12");
-} 
-function letsWave() {
-    wave.classList.add("rotate-12");
-    setTimeout(removeRotate,300)
-}
+// function removeRotate() {
+//     wave.classList.remove("rotate-12");
+// } 
+// function letsWave() {
+//     wave.classList.add("rotate-12");
+//     setTimeout(removeRotate,300)
+// }
