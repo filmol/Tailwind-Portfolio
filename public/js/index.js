72f751ba8dd5c55e7a77d221b8ba121b2c083179
filmol <40041318+filmol@users.noreply.github.com>
2022-05-19 17:59:18 +0200

    Add thesis scroll-hijacking project

--- public/js/index.js
+++ public/js/index.js
@@ -1,63 +1,63 @@
 import Highway from '@dogstudio/highway';
 import Fade from './transition';
-import { addBar } from './transition';
+import { addBar } from './transition';
 import { waves } from './transition';
-import { igAPI } from './transition';
 import { top } from './transition';
 import { checkDarkMode } from './transition';
 
 const H = new Highway.Core({
-    transitions: {
-        default: Fade
-    }
+  transitions: {
+    default: Fade,
+  },
 });
 
 // Variabels and functions for mobile menu
-const mobileBtn = document.querySelector("#mobile-btn");
-mobileBtn.addEventListener("click", showMenu, false);
-const topBtn = document.querySelector("#top-function");
-topBtn.addEventListener("click", top, false);
-const blurContent = document.querySelector(".blur-content");
-const mainContent = document.querySelector("#main-container");
-const navList = document.querySelector(".nav-list");
-const navWidth = document.querySelector("#nav-width");
-const mobileMenu = document.querySelector("#mobile-menu");
-mobileMenu.addEventListener("click", hideMenu, false);
+const mobileBtn = document.querySelector('#mobile-btn');
+mobileBtn.addEventListener('click', showMenu, false);
+const topBtn = document.querySelector('#top-function');
+topBtn.addEventListener('click', top, false);
+const blurContent = document.querySelector('.blur-content');
+const mainContent = document.querySelector('#main-container');
+const navList = document.querySelector('.nav-list');
+const navWidth = document.querySelector('#nav-width');
+const mobileMenu = document.querySelector('#mobile-menu');
+mobileMenu.addEventListener('click', hideMenu, false);
 
 function showMenu(e) {
-    navList.classList.add("h-96", "absolute")
-    blurContent.classList.add("blur")
-    navWidth.classList.remove("w-3/4")
-    mobileMenu.classList.remove("hidden")
-    mainContent.classList.add("relative")
+  navList.classList.add('h-96', 'absolute');
+  blurContent.classList.add('blur');
+  navWidth.classList.remove('w-3/4');
+  mobileMenu.classList.remove('hidden');
+  mainContent.classList.add('relative');
 }
 
 function hideMenu(e) {
-    navList.classList.remove("h-96", "absolute")
-    navWidth.classList.add("w-3/4")
-    mobileMenu.classList.add("hidden")
-    blurContent.classList.remove("blur")
-    mainContent.classList.remove("relative")
-    e.stopPropagation();
-}   
+  navList.classList.remove('h-96', 'absolute');
+  navWidth.classList.add('w-3/4');
+  mobileMenu.classList.add('hidden');
+  blurContent.classList.remove('blur');
+  mainContent.classList.remove('relative');
+  e.stopPropagation();
+}
 
-document.addEventListener('scroll', function(e) {
-    //Displays top-arrow if scroll is below 300.
-    window.scrollY > 300 ? topBtn.classList.remove("hidden") : topBtn.classList.add("hidden");
-})
+document.addEventListener('scroll', function (e) {
+  //Displays top-arrow if scroll is below 300.
+  window.scrollY > 300
+    ? topBtn.classList.remove('hidden')
+    : topBtn.classList.add('hidden');
+});
 
-let darkModeBtn = document.querySelector("#toggleB")
-darkModeBtn.addEventListener('click', function(e) {
-    let html = document.querySelector("#html");
-    if (darkModeBtn.checked) {
-        html.classList.add("dark");
-        checkDarkMode();
-    }    
-    else {
-        html.classList.remove("dark");
-        checkDarkMode();
-    }
-})
+let darkModeBtn = document.querySelector('#toggleB');
+darkModeBtn.addEventListener('click', function (e) {
+  let html = document.querySelector('#html');
+  if (darkModeBtn.checked) {
+    html.classList.add('dark');
+    checkDarkMode();
+  } else {
+    html.classList.remove('dark');
+    checkDarkMode();
+  }
+});
 
 //Displays progress bar and waves when content is loaded.
-document.addEventListener("DOMContentLoaded", () => addBar(), waves(), igAPI());
+document.addEventListener('DOMContentLoaded', () => addBar(), waves());
--- public/js/transition.js
+++ public/js/transition.js
@@ -1,260 +1,274 @@
 import Highway from '@dogstudio/highway';
-import {TimelineLite} from 'gsap';
-import {gsap} from 'gsap';
-import axios from "axios";
+import { TimelineLite } from 'gsap';
+import { gsap } from 'gsap';
+import axios from 'axios';
 
-gsap.config({nullTargetWarn: false});
+gsap.config({ nullTargetWarn: false });
 
-export const addBar = function(){
-    // Animates and fills up the university progress bar gradually.
-    let progress = 0;
-    const graduationDay = new Date("June 30, 2022")
-    const msPerDay = 24 * 60 * 60 * 1000 ; // amount of milliseconds/day
-    let daysLeft = (graduationDay.getTime() - new Date().getTime()) / msPerDay;
-    daysLeft = Math.round(daysLeft);
-    let completed = ((1060-daysLeft)/1060)*100
-    completed = completed.toFixed(0)
+export const addBar = function () {
+  // Animates and fills up the university progress bar gradually.
+  let progress = 0;
+  const graduationDay = new Date('June 30, 2022');
+  const msPerDay = 24 * 60 * 60 * 1000; // amount of milliseconds/day
+  let daysLeft = (graduationDay.getTime() - new Date().getTime()) / msPerDay;
+  daysLeft = Math.round(daysLeft);
+  let completed = ((1060 - daysLeft) / 1060) * 100;
+  completed = completed.toFixed(0);
 
-    const bar = document.getElementById('bar');
-    let progressInterval = setInterval(function(){
-        progress += 0.4;
-        bar.style.width = progress + "%";
-        if(progress >= completed){
-            clearInterval(progressInterval);
-        }
-    }, 8);
-}
+  const bar = document.getElementById('bar');
+  let progressInterval = setInterval(function () {
+    progress += 0.4;
+    bar.style.width = progress + '%';
+    if (progress >= completed) {
+      clearInterval(progressInterval);
+    }
+  }, 8);
+};
 
-export const waves = function(){
-    // Hand animation that waves once the document is loaded.
-    const wave = document.querySelector(".wave");
-    setTimeout(letsWave,3000)
-   
-    const removeRotate = () => wave.classList.remove("rotate-12");
+export const waves = function () {
+  // Hand animation that waves once the document is loaded.
+  const wave = document.querySelector('.wave');
+  setTimeout(letsWave, 3000);
 
-    function letsWave() {
-    wave.classList.add("rotate-12");
-        setTimeout(removeRotate,300)
-    }}
+  const removeRotate = () => wave.classList.remove('rotate-12');
 
-export const igAPI = function () {
-    const options = {
-        method: 'GET',
-        url: 'https://instagram47.p.rapidapi.com/user_posts',
-        params: {username: 'filipmoltzer'},
-        headers: {
-            'x-rapidapi-key': '83323e95f4msh2d60f0bc908b61ep12f88cjsn1fee7db11e56',
-            'x-rapidapi-host': 'instagram47.p.rapidapi.com'
-        }
-    };
-        
-    axios.request(options).then(function (response) {
-        const postnr = Math.floor(Math.random() * 10);
-        document.querySelector("#API-container").children[0].href = "https://www.instagram.com/p/"+response.data.body.items[postnr].code
-        document.querySelector("#API-header").innerHTML=response.data.body.items[postnr].caption.text
-
-        //   ApiContainer.
-    }).catch(function (error) {
-        console.error(error);
-    });
-}
+  function letsWave() {
+    wave.classList.add('rotate-12');
+    setTimeout(removeRotate, 300);
+  }
+};
 
 export const top = function () {
-        // Scrolls back to navigation top
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+  // Scrolls back to navigation top
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+const addKeys = function () {
+  // Animation that slides in the projects name when loaded.
+  setTimeout(function () {
+    const replacers = document.querySelectorAll('[data-replace]');
+    for (let i = 0; i < replacers.length; i++) {
+      let replaceClasses = JSON.parse(
+        replacers[i].dataset.replace.replace(/'/g, '"')
+      );
+      Object.keys(replaceClasses).forEach(function (key) {
+        replacers[i].classList.remove(key);
+        replacers[i].classList.add(replaceClasses[key]);
+      });
     }
+  }, 1);
+};
 
-const addKeys = function(){
-    // Animation that slides in the projects name when loaded.
-    setTimeout(function(){
-        const replacers = document.querySelectorAll('[data-replace]');
-        for(let i=0; i<replacers.length; i++){
-            let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
-            Object.keys(replaceClasses).forEach(function(key) {
-                replacers[i].classList.remove(key);
-                replacers[i].classList.add(replaceClasses[key]);
-            });
-        }
-    }, 1);
-}
-
-const showProjects = function() {
-    // Displays all projects with .show in classlist,
-    // Transforms every other project into a slightly hortisonally displaced position with tailwind classes.
-    const projectList = document.querySelectorAll(".show");   
-    for (let i = 0; i < projectList.length; i++) {
-        projectList[i].classList.add("block", "sm:flex"); 
-        projectList[i].classList.remove("sm:hidden", "hidden");
-        if (i%2>0) {
-            projectList[i].classList.add("sm:-translate-x-36");
-        }
-        else {                 
-            projectList[i].classList.add("sm:translate-x-36");
-        }
+const showProjects = function () {
+  // Displays all projects with .show in classlist,
+  // Transforms every other project into a slightly hortisonally displaced position with tailwind classes.
+  const projectList = document.querySelectorAll('.show');
+  console.log(projectList);
+  for (let i = 0; i < projectList.length; i++) {
+    projectList[i].classList.add('block', 'sm:flex');
+    projectList[i].classList.remove('sm:hidden', 'hidden');
+    if (i % 2 > 0) {
+      projectList[i].classList.add('sm:-translate-x-36');
+    } else {
+      projectList[i].classList.add('sm:translate-x-36');
     }
-}
+  }
+};
 
-let firstVisit = true
-const resetSort = function() {
-    // Resets to default value, or displays all projects if firstVisit
-   
-    const project = document.querySelectorAll(".project");
-    
-    //hide all projects
+let firstVisit = true;
+const resetSort = function () {
+  // Resets to default value, or displays all projects if firstVisit
+
+  const project = document.querySelectorAll('.project');
+
+  //hide all projects
+  for (let i = 0; i < project.length; i++) {
+    project[i].classList.add('sm:hidden', 'hidden');
+    project[i].classList.remove('sm:-translate-x-36', 'sm:translate-x-36');
+    project[i].classList.remove('show');
+  }
+
+  //Display all (if firstvisit)
+  if (firstVisit == true) {
     for (let i = 0; i < project.length; i++) {
-        project[i].classList.add("sm:hidden", "hidden");
-        project[i].classList.remove("sm:-translate-x-36","sm:translate-x-36");
-        project[i].classList.remove("show");
+      project[i].classList.add('show');
     }
-
-    //Display all (if firstvisit)
-    if (firstVisit == true) {
-        for (let i = 0; i < project.length; i++) {
-            project[i].classList.add("show");
-        }
-        firstVisit = false;
-        showProjects();
-    }
-} 
-
-const sort = function(type) {
-    // Adds the .show class to all projects that matches the language value of "type".
-
-    if (type == "all") {
-        firstVisit = true;
-        resetSort();
-    }
-
-    else {
-        resetSort();
-        const langs = document.querySelectorAll(".lang-used");
-        for (let i = 0; i < langs.length; i++) {
-
-            const children = langs[i].childElementCount;
-
-            for (let x = 0; x < children; x++) {
-
-                if (langs[i].children[x].nodeName == "DIV") {
-                    switch(type) {
-                        case langs[i].children[x].firstChild.alt:
-                            let projectContainer = langs[i].children[x].parentElement.parentElement.parentElement;
-                            projectContainer.classList.add("show"); 
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }          
-        }
-    }
+    firstVisit = false;
     showProjects();
-}
+  }
+};
 
-const addListener = function() {
+const sort = function (type) {
+  // Adds the .show class to all projects that matches the language value of "type".
+
+  if (type == 'all') {
     firstVisit = true;
     resetSort();
+  } else {
+    resetSort();
+    const langs = document.querySelectorAll('.lang-used');
+    for (let i = 0; i < langs.length; i++) {
+      const children = langs[i].childElementCount;
 
-    const js = document.querySelector("#javascript");
-    const sql = document.querySelector("#sql");
-    const py = document.querySelector("#python");
-    const all = document.querySelector("#all");
+      for (let x = 0; x < children; x++) {
+        if (langs[i].children[x].nodeName == 'DIV') {
+          switch (type) {
+            case langs[i].children[x].firstElementChild.alt:
+              let projectContainer =
+                langs[i].children[x].parentElement.parentElement.parentElement;
+              projectContainer.classList.add('show');
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
+  }
+  showProjects();
+};
 
-    all.addEventListener('click', function () {
-        addActive(this);
-        sort("all");
-    })
-    js.addEventListener('click', function () {
-        addActive(this);
-        sort("js");
-    })
-    sql.addEventListener('click', function () {
-        addActive(this);
-        sort("sql");
-    })
-    py.addEventListener('click', function () {
-        addActive(this);
-        sort("python");
-    })
-}
+const addListener = function () {
+  firstVisit = true;
+  resetSort();
 
-const addActive = function(lang) {
-    let active = document.querySelector(".active");
-    active.classList.remove("active", "bg-purple-200", "text-gray-800");
-    lang.classList.add("active", "bg-purple-200", "text-gray-800");
+  const js = document.querySelector('#javascript');
+  const sql = document.querySelector('#sql');
+  const py = document.querySelector('#python');
+  const all = document.querySelector('#all');
+
+  all.addEventListener('click', function () {
+    addActive(this);
+    sort('all');
+  });
+  js.addEventListener('click', function () {
+    addActive(this);
+    sort('js');
+  });
+  sql.addEventListener('click', function () {
+    addActive(this);
+    sort('sql');
+  });
+  py.addEventListener('click', function () {
+    addActive(this);
+    sort('python');
+  });
+};
+
+const addActive = function (lang) {
+  let active = document.querySelector('.active');
+  active.classList.remove('active', 'bg-purple-200', 'text-gray-800');
+  lang.classList.add('active', 'bg-purple-200', 'text-gray-800');
 };
 
 export function checkDarkMode() {
-    // Func should be called when darkmode is toggled. add eventlistener.
-    let html = document.querySelector("#html").classList[0];
-    let svg = document.getElementsByClassName("dark:gray-svg");
-    
-    if (html == "dark") {
-        for (let i=0; i<svg.length; i++) {   
-            svg[i].style.fill="rgba(209, 213, 219";
-            }; 
+  // Func should be called when darkmode is toggled. add eventlistener.
+  let html = document.querySelector('#html').classList[0];
+  let svg = document.getElementsByClassName('dark:gray-svg');
+
+  if (html == 'dark') {
+    for (let i = 0; i < svg.length; i++) {
+      svg[i].style.fill = 'rgba(209, 213, 219';
     }
-    else {
-        for (let i=0; i<svg.length; i++) {   
-            svg[i].style.fill="black";
-            }; 
+  } else {
+    for (let i = 0; i < svg.length; i++) {
+      svg[i].style.fill = 'black';
     }
+  }
 }
 
-
-class Fade extends Highway.Transition{
-    // Animation to change between subpages without reloading the doc, also includes an added animation transition.
-    in({from, to, done}){
+class Fade extends Highway.Transition {
+  // Animation to change between subpages without reloading the doc, also includes an added animation transition.
+  in({ from, to, done }) {
     const tl = new TimelineLite();
-    tl.fromTo(to, 0.5, {left:'0%', top: '-100%'}, {top:'5rem',})
-    .fromTo( 
-    to,
-    0.5,
-    {height:'0vh'},
-    {height:'90vh', top:'5rem', 
-    onComplete: function(){
-        // Runs the right animations on the right page.
-        if (to.classList.contains("home")) {
-            setTimeout(addBar,3000)
-            addKeys();
-            document.querySelector("#nav-underline").classList.remove("margin-contact","margin-about","margin-skills", "margin-projects");           
-            document.querySelector("#nav-underline").classList.add("margin-about");           
-            waves();
-            igAPI();
-        } 
-        if (to.classList.contains("projects")) {
-            addKeys();
-            document.querySelector("#nav-underline").classList.remove("margin-contact","margin-about","margin-skills", "margin-projects");           
-            document.querySelector("#nav-underline").classList.add("margin-projects");
-            addListener();
-            checkDarkMode();
-            } 
-        if (to.classList.contains("skills")) {
-            addKeys();
-            document.querySelector("#nav-underline").classList.remove("margin-contact","margin-about","margin-skills", "margin-projects");           
-            document.querySelector("#nav-underline").classList.add("margin-skills");
-            checkDarkMode();
-            } 
-        if (to.classList.contains("contact")) {
-            addKeys();
-            document.querySelector("#nav-underline").classList.remove("margin-contact","margin-about","margin-skills", "margin-projects");           
-            document.querySelector("#nav-underline").classList.add("margin-contact");
-            checkDarkMode();
-        }
-        
-        //Scroll to top as default for each page
-        top();
+    tl.fromTo(to, 0.5, { left: '0%', top: '-100%' }, { top: '5rem' })
+      .fromTo(
+        to,
+        0.5,
+        { height: '0vh' },
+        {
+          height: '90vh',
+          top: '5rem',
+          onComplete: function () {
+            // Runs the right animations on the right page.
+            if (to.classList.contains('home')) {
+              setTimeout(addBar, 3000);
+              addKeys();
+              document
+                .querySelector('#nav-underline')
+                .classList.remove(
+                  'margin-contact',
+                  'margin-about',
+                  'margin-skills',
+                  'margin-projects'
+                );
+              document
+                .querySelector('#nav-underline')
+                .classList.add('margin-about');
+              waves();
+            }
+            if (to.classList.contains('projects')) {
+              addKeys();
+              document
+                .querySelector('#nav-underline')
+                .classList.remove(
+                  'margin-contact',
+                  'margin-about',
+                  'margin-skills',
+                  'margin-projects'
+                );
+              document
+                .querySelector('#nav-underline')
+                .classList.add('margin-projects');
+              addListener();
+              checkDarkMode();
+            }
+            if (to.classList.contains('skills')) {
+              addKeys();
+              document
+                .querySelector('#nav-underline')
+                .classList.remove(
+                  'margin-contact',
+                  'margin-about',
+                  'margin-skills',
+                  'margin-projects'
+                );
+              document
+                .querySelector('#nav-underline')
+                .classList.add('margin-skills');
+              checkDarkMode();
+            }
+            if (to.classList.contains('contact')) {
+              addKeys();
+              document
+                .querySelector('#nav-underline')
+                .classList.remove(
+                  'margin-contact',
+                  'margin-about',
+                  'margin-skills',
+                  'margin-projects'
+                );
+              document
+                .querySelector('#nav-underline')
+                .classList.add('margin-contact');
+              checkDarkMode();
+            }
 
-        from.remove();
-        done(); 
-    }}
-    )
-    .fromTo(to.children[0], 2, {opacity: 0},{opacity: 1})
-    .fromTo(to.children[1], 2, {opacity: 0},{opacity: 1})
-    }
-    out({from,done}){
-        done();
-    }
+            //Scroll to top as default for each page
+            top();
+
+            from.remove();
+            done();
+          },
+        }
+      )
+      .fromTo(to.children[0], 2, { opacity: 0 }, { opacity: 1 })
+      .fromTo(to.children[1], 2, { opacity: 0 }, { opacity: 1 });
+  }
+  out({ from, done }) {
+    done();
+  }
 }
 
 export default Fade;
